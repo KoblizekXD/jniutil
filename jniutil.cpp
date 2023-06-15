6e657bd0da54538096439ded69fab33df6828208
KoblizekXD <janprokupek@centrum.cz>
2023-06-15 18:45:16 +0200

    added: class creating

--- jniutil.cpp
+++ jniutil.cpp
@@ -1,5 +1,23 @@
 #include "jniutil.h"
 
 #include <iostream>
+#include <jni.h>
 
+using std::string;
 
+JNIEnv *jni_env;
+
+// TODO: fix signatures
+jobject create_class_instance(jclass type, jobject params...) {
+    return jni_env->NewObject(type,
+                              jni_env->GetMethodID(type, "<init>", ""),
+                              params);
+}
+
+void env(JNIEnv *env) {
+    jni_env = env;
+}
+
+jclass find_class(string name) {
+    return jni_env->FindClass(name.c_str());
+}
--- jniutil.h
+++ jniutil.h
@@ -1,6 +1,13 @@
-#ifndef JNIUTIL_JNIUTIL_H
-#define JNIUTIL_JNIUTIL_H
+#ifndef JNIUTIL_H
+#define JNIUTIL_H
 
+#include <jni.h>
+#include <iostream>
 
+using std::string;
 
-#endif //JNIUTIL_JNIUTIL_H
+jclass find_class(string name);
+void env(JNIEnv* env);
+jobject create_class_instance(jclass type, jobject params...);
+
+#endif // JNIUTIL_H
